9192acc34849df481961e53bc0530ebc95a465d5
Norberto Padin <npadin@gmail.com>
2024-12-02 18:20:55 -0300

    new files

--- dna.py
+++ dna.py
@@ -1,27 +1,53 @@
 import requests
-
-'''
-class CatalystCenter(self):
-    def __init__(self):
-        self.token = self.get_token()
-        self.url = "https://10.1.100.10/dna/system/api/v1/network-device"
-        self.headers = {
-            "X-Auth-Token": self.token,
-            "Content-Type": "application/json"
-        }
-
-        '''
+from requests.auth import HTTPBasicAuth
+from dnac_config import USER, PASSWORD, DNAC
+import urllib3
+import logging
+import os
+from datetime import datetime
+import pprint
 
 
-def get_token(self):
+logger: logging.Logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('dnac.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def get_token():
     token = requests.post(
-        "https://10.1.100.10/dna/system/api/v1/auth/token",
+        f'https://{DNAC}/dna/system/api/v1/auth/token',
         auth=HTTPBasicAuth(
-            username="admin",
-            password="BvsTv3965!"
+            username=USER,
+            password=PASSWORD
         ),
         headers={'content-type': 'application/json'},
         verify=False,
     )
     data = token.json()
     return data["Token"]
+
+
+def get_device_from_dnac(device_ip):
+    response = requests.get(
+        f'https://{DNAC}/dna/intent/api/v1/network-device/ip-address/{device_ip}',
+        headers={
+            'X-Auth-Token': '{}'.format(get_token()),
+            'Content-type': 'application/json',
+        },
+        verify=False
+    )
+    return response.json()
+
+
+def main() -> None:
+    pprint.pp(get_device_from_dnac('10.1.100.3'))
+
+
+if __name__ == "__main__":
+    main()
---
+++ dnac_config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+    _summary_
+    Credenciales del Cisco Catalyst Control Center para usuario API.
+    DNAC >> URL base para obtener información vía APIs.
+    SUBDIR >> Directorio donde se bajan los running config
+'''
+
+USER = 'admin'
+PASSWORD = 'BvsTv3965!'
+DNAC = '10.1.100.10'
+SUBDIR = 'configurations/'
