35824fb407e776961ae5ef3ef15ce4bffe256318
Norberto Padin <npadin@gmail.com>
2024-12-06 17:16:52 -0300

    updated files

--- dnac_config.py
+++ dnac_config.py
@@ -8,7 +8,7 @@
     SUBDIR >> Directorio donde se bajan los running config
 '''
 
-USERNAME = 'admin'
+USER = 'admin'
 PASSWORD = 'BvsTv3965!'
-BASE_URL = r'https://10.1.100.10'
+DNAC = "10.1.100.10"
 SUBDIR = 'configurations/'
--- dnac_oop.py
+++ dnac_oop.py
@@ -1,6 +1,5 @@
 import requests
 from requests.auth import HTTPBasicAuth
-from dnac_config import USERNAME, PASSWORD, BASE_URL
 import urllib3
 import logging
 import os
@@ -8,6 +7,14 @@ import time
 from datetime import datetime
 import pprint
 import json
+import os
+import sys
+from pathlib import Path
+import logging
+from tqdm import tqdm, trange
+from typing import Optional
+from io import StringIO
+import argparse
 
 
 logger: logging.Logger = logging.getLogger(__name__)
@@ -21,11 +28,26 @@ logger.addHandler(file_handler)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+class TQDMStreamHandler(logging.StreamHandler):
+    def __init__(self, stream=sys.stdout):
+        super().__init__(stream)
+        self._original_stream = stream
+        self._capture_stream = StringIO()
+
+    def emit(self, record):
+        if tqdm._instances:
+            self._capture_stream.write(self.format(record) + "\n")
+        else:
+            super().emit(record)
+
+    def get_log_content(self):
+        return self._capture_stream.getvalue()
+
 class Dnac():
-    def __init__(self,  dnac_url, dnac_user, dnac_password) -> None:
+    def __init__(self,  dnac_ip, dnac_user, dnac_password) -> None:
         self.dnac_user = dnac_user
         self.dnac_password = dnac_password
-        self.base_url = dnac_url
+        self.base_url = f"https://{dnac_ip}"
         self.auth_url = r'/dna/system/api/v1/auth/token'
         self.devices = {}
         self.sites = []
@@ -168,8 +190,63 @@ class Dnac():
         pass
 
 
+def setup_logger(log_file: str = "prime.log") -> logging.Logger:
+    if log_file is None:
+        log_file = f"prime_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+    logger: logging.Logger = logging.getLogger('myLogger')
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(log_file, mode='w')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    log_handler = TQDMStreamHandler(sys.stdout)
+    log_handler.setFormatter(formatter)
+    logger.addHandler(log_handler)
+
+    return logger
+
+def parse_args():
+    # Import default values from config
+    from dnac_config import USER, PASSWORD, DNAC
+
+    parser = argparse.ArgumentParser(
+        description="Cisco Prime API Interactor Script"
+    )
+    parser.add_argument(
+        "--dnac-ip",
+        default=DNAC,  # Use default from pi_config.py if not provided
+        help=f"IP address or hostname of the Cisco Prime server (default: {DNAC})"
+    )
+    parser.add_argument(
+        "--username",
+        default=USER,  # Use default from pi_config.py if not provided
+        help=f"Username for API authentication (default: {USER})"
+    )
+    parser.add_argument(
+        "--password",
+        default=PASSWORD,  # Use default from pi_config.py if not provided
+        help="Password for API authentication (default: value from pi_config.py)"
+    )
+    parser.add_argument(
+        "--log-file",
+        default="prime.log",
+        help="Path to the log file (default: prime.log)"
+    )
+    parser.add_argument(
+        "--max-results",
+        type=int,
+        default=1000,
+        help="Maximum number of results per API call (default: 1000)"
+    )
+    return parser.parse_args()
+
+
 def main() -> None:
-    dnac = Dnac(BASE_URL, USERNAME, PASSWORD)
+    args: argparse.Namespace = parse_args()
+    dnac = Dnac(args.dnac_ip, args.username, args.password)
     print(f'Cantidad de dispositivos: {dnac.get_device_count()}')
     pprint.pp(dnac.get_all_devices())
     pprint.pp(dnac.get_devices_by_platfom('AIR-AP2802I-A-K9'))
@@ -177,4 +254,6 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        logger: logging.Logger = setup_logger()
+        main()
